6559e2c320266b90e02a2c219b40babb830e9ab5
marianosim <mariano.simantob@gmail.com>
2023-05-20 13:36:59 -0300

    OrderItem component created. Order layout & styles.

---
+++ src/components/order-item/index.jsx
@@ -0,0 +1,29 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { styles } from './styles';
+import { COLORS } from '../../constants';
+
+const OrderItem = ({ item, onRemove }) => {
+  const formatDate = (time) => {
+    const date = new Date(time);
+    return date.toLocaleDateString();
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.date}>{formatDate(item.date)}</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <View style={styles.body}>
+          <Text style={styles.total}>U$D {item.total}</Text>
+        </View>
+        <TouchableOpacity onPress={() => onRemove(item.id)}>
+          <Ionicons name="trash" size={22} color={COLORS.brightRed} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default OrderItem;
